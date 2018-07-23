bbd64da897aa36f284459b755a933304f8cd14e2
ayushaggarwal <ayushagrawal13@gmail.com>
2018-07-23 21:30:43 +0530

    Fixed last signin test case with frame handling

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -5,9 +5,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class SignInTest {
+public class SignInTest extends BaseTest{
 
-    WebDriver driver = new ChromeDriver();
+   
 
     @Test
     public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
@@ -19,32 +19,14 @@ public class SignInTest {
 
         driver.findElement(By.linkText("Your trips")).click();
         driver.findElement(By.id("SignIn")).click();
-
+        // aayush. : we need to switch to frame here. iframe[name='modal_window']
+        driver.switchTo().frame("modal_window");
+        
         driver.findElement(By.id("signInButton")).click();
-
+       
         String errors1 = driver.findElement(By.id("errors1")).getText();
         Assert.assertTrue(errors1.contains("There were errors in your submission"));
         driver.quit();
-    }
-
-    private void waitFor(int durationInMilliSeconds) {
-        try {
-            Thread.sleep(durationInMilliSeconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
-
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
     }
 
 
